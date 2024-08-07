51fb71168a74081cd557bea20c57301fcd091d4f
Marcos Medel <mmedel888@gmail.com>
2024-08-07 13:27:10 -0400

    added information about the team

---
+++ src/contents/servicios-inmigración.ts
@@ -0,0 +1,43 @@
+interface ServicioInmigracion {
+    name: string;
+    description: string;
+    imgUrl: string;
+}
+
+export const ServicioInmigracionList: ServicioInmigracion[] = [
+    {
+        name: 'Permiso de trabajo',
+        description: '¿Desea obtener un permiso de trabajo en los Estados Unidos? ¡Infórmese aquí!',
+        imgUrl: '/images/inmigracion/001.webp'
+    },
+    {
+        name: 'Ajuste de Estatus (Residencia Permanente)',
+        description: 'Todo lo que debe saber para optar por su Ajuste de Status Migratorio.',
+        imgUrl: '/images/inmigracion/001.webp'
+    },
+    {
+        name: 'Naturalización (Ciudadanía)',
+        description: 'Requisitos generales para su trámite de Naturalización en los Estados Unidos.',
+        imgUrl: '/images/inmigracion/001.webp'
+    },
+    {
+        name: 'Peticiones de Asilo',
+        description: 'En sus dos Modalidades (Afirmativo y Defensivo)',
+        imgUrl: '/images/inmigracion/001.webp'
+    },
+    {
+        name: 'Peticiones Familiares',
+        description: 'Conozca los detalles y requerimientos para realizar una petición familiar.',
+        imgUrl: '/images/inmigracion/001.webp'
+    },
+    {
+        name: 'Parole Humanitario',
+        description: '¿Qué debe tener para optar por el Parole Humanitario?',
+        imgUrl: '/images/inmigracion/001.webp'
+    },
+    {
+        name: 'Perdones de Pago (Fee Waiver)',
+        description: 'Información necesaria para la solicitud del perdón de pago.',
+        imgUrl: '/images/inmigracion/001.webp'
+    }
+];
